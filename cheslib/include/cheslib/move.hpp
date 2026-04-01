a44aa91ce16311d8d743b73f399fb37e8916c1a0
karas <141109026+Ka-raS@users.noreply.github.com>
2026-04-02 01:27:51 +0700

    16bit state and hiccups

--- cheslib/include/cheslib/move.hpp
+++ cheslib/include/cheslib/move.hpp
@@ -24,14 +24,16 @@ enum MoveFlag : uint8_t {
     QUEEN_PROMO_CAPTURE = 15
 };
 
-// 16-bit encoded move
-// bit layout:
-// 0-5   : from square
-// 6-11  : to square
-// 12-13 : promotion piece
-// 14    : is capture
-// 15    : is promotion
-// see more here: https://www.chessprogramming.org/Encoding_Moves
+/**
+ * 16bit encoded move
+ * bit layout:
+ *  0-5   : from square
+ *  6-11  : to square
+ *  12-13 : promotion piece
+ *  14    : is capture
+ *  15    : is promotion
+ * see more here: https://www.chessprogramming.org/Encoding_Moves
+ */
 class Move {
   public:
     constexpr Move(Square from, Square to, MoveFlag flag) : _data(from | (to << 6) | (flag << 12)) {
@@ -39,6 +41,8 @@ class Move {
         assert(to < SQUARE_CNT);
         assert(flag <= QUEEN_PROMO_CAPTURE);
     }
+
+    ~Move() = default;
 
     constexpr Square from() const {
         return Square(_data & 0b111111);
@@ -71,7 +75,7 @@ class Move {
     }
 
   private:
-    const uint16_t _data;
+    uint16_t _data;
 };
 
 } // namespace cheslib
